9d12b6a7a5ed896ec4c13bf1596a2a551bdf9570
user <rikki@ubuntu.(none)>
2014-06-15 14:57:30 +0400

    add unit tests

--- pynard.py
+++ pynard.py
@@ -1,9 +1,12 @@
+PYNARD_VER = "0.1"
+
 #imports
 import copy
 import random
 
 #constants
 BG_FIELD_SIZE = 24
+BG_END_INDEX = BG_FIELD_SIZE - 1
 BG_USER_CHECKERS_COUNT = 15
 
 BG_START_HOUSE_END = 5
@@ -26,6 +29,9 @@ class Playboard:
     def __ne__(self, obj):
         return not self == obj
 
+    def __str__(self):
+        return str(self.fields) + " - %s . %s" % (self.player1_stack, self.player2_stack)
+
 MOV_DEFAULT, MOV_OUT = 0, 1
 
 class PlayboardController:
@@ -37,16 +43,10 @@ class PlayboardController:
         if player == PLAYER1:
             return pos
         elif player == PLAYER2:
-            return BG_FIELD_SIZE - pos - 1
-
-    def player_get_stack(self, playboard, player):
-        if player == PLAYER1:
-            return playboard.player1_stack
-        elif player == PLAYER2:
-            return playboard.player2_stack
+            return BG_END_INDEX - pos
 
     def _get_raw(self, playboard, player, pos):
-        pos = _pos_by_player(playboard, player, pos)
+        pos = self._pos_by_player(player, pos)
         return playboard.fields[pos]
 
     def _get_by_player(self, playboard, player, pos):
@@ -61,27 +61,33 @@ class PlayboardController:
         board.fields = data
         return board
 
+    def player_get_stack(self, playboard, player):
+        if player == PLAYER1:
+            return playboard.player1_stack
+        elif player == PLAYER2:
+            return playboard.player2_stack
+
     def player_stack_inc(self, playboard, player):
         if player == PLAYER1:
-            playboard.player1 += 1
+            playboard.player1_stack += 1
         elif player == PLAYER2:
-            playboard.player2 += 1
+            playboard.player2_stack += 1
 
     def get(self, playboard, player, pos):
-        return _get_by_player(playboard, player, pos)
+        return self._get_by_player(playboard, player, pos)
 
     def move_to(self, playboard, player, pos):
-        inc_val = _inc_by_player(player)
-        pos_from = _pos_by_player(playboard, player, pos)
-        playtable.fields[pos] += inc_val
+        inc_val = self._inc_by_player(player)
+        pos_from = self._pos_by_player(player, pos)
+        playboard.fields[pos_from] += inc_val
 
     def move_from(self, playboard, player, pos):
-        inc_val = _inc_by_player(player)
-        pos_from = _pos_by_player(playboard, player, pos)
-        playtable.fields[pos] -= inc_val
+        inc_val = self._inc_by_player(player)
+        pos_from = self._pos_by_player(player, pos)
+        playboard.fields[pos_from] -= inc_val
 
     def get_player(self, playboard, player, pos):
-        val = _get_raw(playboard, player, pos)
+        val = self._get_raw(playboard, player, pos)
         if val > 0:
             return PLAYER1
         if val < 0:
@@ -91,95 +97,93 @@ class PlayboardController:
 
 class RulesController:
     start_exceptional = [ {6, 6}, {4, 4}, {3, 3} ]
-    init_position = [BG_USER_CHECKERS_COUNT] + [0] * (BG_FIELD_SIZE - 2) + [-BG_USER_CHECKERS_COUNT]
+    init_position = [BG_USER_CHECKERS_COUNT] + [0] * (BG_END_INDEX - 1) + [-BG_USER_CHECKERS_COUNT]
 
     def __init__(self, playboard_controller_item):
         self._playboard_controller = playboard_controller_item
 
-    def _init_move(self, playboard, player, pos, step):
-        return Move(deepcopy(playboard), player, pos, step)
+    def _init_playboard(self, playboard):
+        return copy.deepcopy(playboard)
 
     def set_start(self):
-        new_board = Playboard()
-        _playboard_controller.set_start(new_board)
-        return new_board
+        return self._playboard_controller.init_start(copy.deepcopy(self.init_position))
 
     def is_win(self, playboard, player):
-        if _play_table_controller.player_stack_var(playboard, player) >= BG_USER_CHECKERS_COUNT:
+        if self._playboard_controller.player_get_stack(playboard, player) >= BG_USER_CHECKERS_COUNT:
             return WIN
         return NO_WIN
 
     def get_game_status(self, playboard, player):
-        _play_table_controller_get = _play_table_controller.get
+        _play_table_controller_get = self._playboard_controller.get
+        if self._playboard_controller.player_get_stack(playboard, player):
+            return GAMESTATUS_END
         if _play_table_controller_get(playboard, player, 0) == BG_USER_CHECKERS_COUNT:
             return GAMESTATUS_START
         checkers_count = 0
-        for iterator in range(BG_END_HOUSE_START, BG_FIELD_SIZE): checkers_count += _play_table_controller_get(playboard, player, iterator)
+        for iterator in range(BG_END_HOUSE_START, BG_FIELD_SIZE):
+            val = _play_table_controller_get(playboard, player, iterator)
+            if val > 0: checkers_count += val
+            if checkers_count == BG_USER_CHECKERS_COUNT: break
         if checkers_count == BG_USER_CHECKERS_COUNT:
             return GAMESTATUS_END
         return GAMESTATUS_MIDDLE
 
-    def do_move(self, playboard, player, pos, step):
-        play_table_status = get_game_status(playboard, player)
-        if play_table_status == GAMESTATUS_START and can_move_start(playboard, player, pos, step):
-            return do_move_start(playboard, player, pos, step)
-        if play_table_status == GAMESTATUS_MIDDLE and can_move_middle(playboard, player, pos, step):
-            return do_move_middle(playboard, player, pos, step)
-        elif play_table_status == GAMESTATUS_END and can_move_end(playboard, player, pos, step):
-            return do_move_end(playboard, player, pos, step)
-        return None
-
     def can_move(self, playboard, player, pos, step):
-        play_table_status = get_game_status(playboard, player)
+        play_table_status = self.get_game_status(playboard, player)
         if play_table_status == GAMESTATUS_START:
-            return can_move_start(playboard, player, pos, step)
+            return self.can_move_start(playboard, player, pos, step)
         if play_table_status == GAMESTATUS_MIDDLE:
-            return can_move_middle(playboard, player, pos, step)
+            return self.can_move_middle(playboard, player, pos, step)
         elif play_table_status == GAMESTATUS_END:
-            return can_move_end(playboard, player, pos, step)
+            return self.can_move_end(playboard, player, pos, step)
         return False
 
-    def can_move_start(self, playboard, player, pos, step):
-        return pos == 0
+    def do_move(self, playboard, player, pos, step):
+        play_table_status = self.get_game_status(playboard, player)
+        if play_table_status == GAMESTATUS_START and self.can_move_start(playboard, player, pos, step):
+            return self.do_move_start(playboard, player, pos, step)
+        if play_table_status == GAMESTATUS_MIDDLE and self.can_move_middle(playboard, player, pos, step):
+            return self.do_move_middle(playboard, player, pos, step)
+        elif play_table_status == GAMESTATUS_END and self.can_move_end(playboard, player, pos, step):
+            return self.do_move_end(playboard, player, pos, step)
+        return None
 
-    def _is_this_player(self, player, pos):
-        field_player = _playboard_controller.get_player(playboard, player, pos)
-        return field_player == NO_PLAYER and field_player != player
+    def _is_this_player(self, playboard, player, pos):
+        field_player = self._playboard_controller.get_player(playboard, player, pos)
+        return field_player == player
 
-    def _is_enemy_player(self, player, pos):
-        field_player = _playboard_controller.get_player(playboard, player, pos)
+    def _is_enemy_player(self, playboard, player, pos):
+        field_player = self._playboard_controller.get_player(playboard, player, pos)
         return field_player != NO_PLAYER and field_player != player
 
     def _is_before_border(self, pos, step):
         return pos + step < BG_FIELD_SIZE
 
+    def can_move_start(self, playboard, player, pos, step):
+        return pos == 0
+
     def can_move_middle(self, playboard, player, pos, step):
-        return _is_this_player(player, pos) and _is_before_border(pos, step) and not _is_enemy_player(pos + step)
+        return self._is_this_player(playboard, player, pos) and self._is_before_border(pos, step) and not self._is_enemy_player(playboard, player, pos + step)
 
     def can_move_end(self, playboard, player, pos, step):
-        return not  _is_this_player(player, pos) and _is_enemy_player(pos + step)
+        return self._is_this_player(playboard, player, pos) and (not self._is_before_border(pos, step) or not self._is_enemy_player(playboard, player, pos + step))
 
     def do_move_start(self, playboard, player, pos, step):
-        return do_move_middle(playboard, player, pos, step)
+        return self.do_move_middle(playboard, player, pos, step)
 
     def do_move_middle(self, playboard, player, pos, step):
-        move_item = _init_move(playboard, player, pos, step)
-        _playboard_controller.move_from(move_item.playboard, player, pos)
-        _playboard_controller.move_to(move_item.playboard, player, pos + step)
-        return move_item
+        playboard_new = self._init_playboard(playboard)
+        self._playboard_controller.move_from(playboard_new, player, pos)
+        self._playboard_controller.move_to(playboard_new, player, pos + step)
+        return playboard_new
 
     def do_move_end(self, playboard, player, pos, step):
-        if _is_before_border(pos, step):
-            return do_move_middle(playboard, player, pos, step)
-        move_item = _init_move(playboard, player, pos, step)
-        _playboard_controller.move_from(move_item.playboard, player, pos)
-        _playboard_controller.player_stack_inc(move_item.playboard, player)
-        return move_item
-
-    def dice_moves(self, dice):
-        if dice[0] == dice[1]:
-            return dice * 2
-        return dice
+        if self._is_before_border(pos, step):
+            return self.do_move_middle(playboard, player, pos, step)
+        playboard_new = self._init_playboard(playboard)
+        self._playboard_controller.move_from(playboard_new, player, pos)
+        self._playboard_controller.player_stack_inc(playboard_new, player)
+        return playboard_new
 
 class DiceController (object):
     @staticmethod
@@ -192,29 +196,40 @@ class DiceController (object):
         j = 1
         dices = []
         while i < numbers:
-            while j < numbers:
-                dices.add((i, j))
+            dices.add({i, i, i, i})
+            while j < i:
+                dices.add({i, j})
                 j+=1
             i+=1
         return dices
 
-class Move:
-    def __init__(self, playboard_init, player_init, pos_init, step_init):
-        playboard_ = playboard_init
-        player = player_init
-        pos = pos_init
-        step = step_init
+    @staticmethod
+    def dice_probability(self, dice):
+        if dice[0] == dice[1]:
+            return 1/36
+        else:
+            return 2/36
 
+class MoveNetItem:
+    moves = []
     player = NO_PLAYER
-
-    pos = -1
-    step = 0
     playboard = None
 
     parent = None
     children = None
 
     estimation = 0.0
+    probability = 0.0
 
     def __str__(self):
-        return "%s - %s [%s]" % (pos, step, estimation)
+        return "est: %f, prob: %f: " % (estimation, probability) + ','.join([PlayboardVisualiser.show_move(move) for move in moves])
+
+class PlayboardVisualiser:
+    @staticmethod
+    def show_move(move_tuple):
+        if x[1] <= 0:
+            return "%s - =>" % (x[0])
+        return "%s - %s" % (x[0], x[1])
+
+if __name__ == "__main__":
+    print("PyNard %s by rikkimongoose (http://github.com/rikkimongoose/pynard/)" % PYNARD_VER)
